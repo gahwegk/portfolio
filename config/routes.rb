6300b698d163e5133a52447351e4f1c01d5179ce
mina <minami.o2684@gmail.com>
2023-03-21 09:04:02 +0000

    必要なファイルの準備:deviseのインストール、model,controller,viewファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about', to: 'homes#about'
+
 end
