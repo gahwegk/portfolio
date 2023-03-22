d9a8ecbe620ae80f5a8394d9a7f5557b4b08d1a0
mina <minami.o2684@gmail.com>
2023-03-22 02:18:42 +0000

    ログイン時エラー解消
    nameカラムのnillが原因でログインできず
    
    devise.rbのconfig.authentication_keysをnameに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
-
   resources :users, only: [:index, :show, :edit, :update]
   resources :reviews, only:[:index, :show, :edit, :new, :create, :update, :destroy]
 end
