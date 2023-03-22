97f9923b0abf0de29a599bf146e8cd91b9c262e9
mina <minami.o2684@gmail.com>
2023-03-22 08:45:46 +0000

    コントローラーアクション記述
    user, reviewコントローラーの記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,58 @@
 class ReviewsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def index
+    @reviews = Review.all
+    @review = Review.new
   end
 
   def show
+    @review = Review.find(params[:id])
+    @user = @review.user
   end
 
-
   def edit
+    @review = Review.find(params[:id])
+    unless @review.user == current_user
+      redirect_to reviews_path
+    end
   end
   
   def new
+    @review = Review.new
   end
   
   def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @reviews = Review.all
+    if @review.save
+     redirect_to review_path(@review), notice: 'You have created review successfully.'
+    else
+     render :index
+    end 
   end
   
   def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+    redirect_to review_path(@review), notice: 'You have updated review successfully.'
+    else
+    render :edit
+    end
+  
   end
   
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
   end
+  
+  private
+  
+  def review_params
+    params.require(:review).permit(:title, :review)
+  end  
 end
