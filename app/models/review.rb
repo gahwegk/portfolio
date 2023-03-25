8a4a6cd5855bed1dc32a31f744198cd80d69e75f
mina <minami.o2684@gmail.com>
2023-03-25 13:38:03 +0000

    コメント機能設計の準備
    modelの作成, アソシエーションの記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :review, presence: true
+  
+  has_many :post_comments, dependent: :destroy
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
+  
   has_many :reviews, dependent: :destroy
-  validates :name, length: { minimum: 2, maximum: 20 }, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  
+  has_many :post_comments, dependent: :destroy
 end
