1de0a23f5cd58dc657d5f2d009371f69a3fd036f
mina <minami.o2684@gmail.com>
2023-04-12 12:39:37 +0000

    categoryモデル、db作成

---
+++ db/migrate/20230412123704_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :categoryname
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+    
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_122451) do
+ActiveRecord::Schema.define(version: 2023_04_12_123704) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "categoryname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
