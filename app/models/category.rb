15c75df4b4c95439a756992096a357193c3fb120
mina <minami.o2684@gmail.com>
2023-04-12 12:46:07 +0000

    アソシエーション記述

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :reivew_categories
+  has_many :reviews, through: :review_categories
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,9 @@ class Review < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  has_many :review_categories
+  has_many :categories, through: :review_categories
+  
   def favorited_by?(user)
    favorites.where(user_id: user.id).exists?
   end
