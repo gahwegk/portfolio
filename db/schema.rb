f76cae133fde37c5ee63f163cd2d34e57e7cc7d3
mina <minami.o2684@gmail.com>
2023-04-12 09:55:43 +0000

    db:マイグレーションファイル整理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_073828) do
+ActiveRecord::Schema.define(version: 2023_03_26_001840) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
